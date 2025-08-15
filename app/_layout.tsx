a3cd3e9c4911a6c7e499680bbe7ef95da4c4fa08
aladdin4u <taofikakinkumi@gmail.com>
2025-08-15 11:00:16 +0100

    feat: splash screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,11 +6,30 @@ import { MD3LightTheme as DefaultTheme, PaperProvider } from 'react-native-paper
 import 'react-native-reanimated';
 
 import { Colors } from '@/constants/Colors';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+
+// Set the animation options. This is optional.
+SplashScreen.setOptions({
+  duration: 1000,
+  fade: true,
+});
 
 export default function RootLayout() {
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hide();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    // Async font loading only occurs in development.
+    return null;
+  }
 
   const theme = {
     ...DefaultTheme,
@@ -19,11 +38,6 @@ export default function RootLayout() {
       primary: Colors.primary,
     },
   };
-
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
 
   return (
     <PaperProvider theme={theme}>
